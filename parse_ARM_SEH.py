62ee30ad22906e5a0a1ac5e6097b89590988722b
tandasat <tanda.sat@gmail.com>
2015-01-25 15:23:05 -0800

    Fix wrong names

--- parse_ARM_SEH.py
+++ parse_ARM_SEH.py
@@ -90,53 +90,53 @@ class XdataRecord(object):
 
 
 class ExceptionHandlerInformation(object):
-    '''Represents Exception Handler Information'''
+    '''Represents Exception Handler Information (a.k.a, SCOPE_TABLE)'''
     def __init__(self, address):
         self.address = address
         self.exp_handler = Dword(address) + idaapi.get_imagebase()
-        self.number_of_scope_tables = Dword(address + 4)
-        self.address_of_scope_tables = address + 8
-        self.scope_tables = []
+        self.number_of_scope_entries = Dword(address + 4)
+        self.address_of_scope_entries = address + 8
+        self.scope_entries = []
         # Some handlers have huge values such as 0xffffffe9 and are not
         # supported.
-        if self.number_of_scope_tables > 0xff000000:
+        if self.number_of_scope_entries > 0xff000000:
             return
-        for i in range(0, self.number_of_scope_tables):
-            self.scope_tables.append(
-                ScopeTable(self.address_of_scope_tables + i * 16))
+        for i in range(0, self.number_of_scope_entries):
+            self.scope_entries.append(
+                ScopeEntry(self.address_of_scope_entries + i * 16))
 
     def apply_to_database(self):
         _make_references(self.address, self.exp_handler, 'Handler ')
         MakeDword(self.address + 4)
-        # Since nested SEH blocks show up first in the list of tables, this
-        # reverse makes comments prettier like that:
+        # Since nested SEH blocks show up first in the table, this reversing
+        # makes comments prettier like this:
         # __try{ // outside SEH
         # __try{ // nested SEH
         # } // nested SEH
         # } // outside SEH
-        for table in reversed(self.scope_tables):
-            table.apply_to_database()
+        for entry in reversed(self.scope_entries):
+            entry.apply_to_database()
 
 
-class ScopeTable(object):
-    '''Represents a SCOPE_TABLE'''
+class ScopeEntry(object):
+    '''Represents an entry of SCOPE_TABLE'''
     def __init__(self, address):
         if Dword(address + 8) == 1:
             # Filter may have 1 in it. This is invalid and this code handle it
             # as __try/__except but without a valid except filter information.
-            self.table = TryInvalidExceptTable(address)
+            self.entry = TryInvalidExceptEntry(address)
         elif Dword(address + 12) == 0:
             # It is __try/__finally when Target has no value.
-            self.table = TryFinallyTable(address)
+            self.entry = TryFinallyEntry(address)
         else:
             # It is __try/__except when Filter and Target have valid values.
-            self.table = TryExceptTable(address)
+            self.entry = TryExceptEntry(address)
 
     def apply_to_database(self):
-        self.table.apply_to_database()
+        self.entry.apply_to_database()
 
 
-class SEHTable(object):
+class SEHEntry(object):
     '''Implements common things for an SEH SCOPE_TABLE'''
     def __init__(self, address):
         self.address = address
@@ -148,13 +148,13 @@ class SEHTable(object):
         _make_references(self.address + 4, self.end, '} //try ')
 
 
-class TryExceptTableBase(SEHTable):
+class TryExceptEntryBase(SEHEntry):
     '''Implements common things for a __try/__except style SCOPE_TABLE'''
     def __init__(self, address):
-        super(TryExceptTableBase, self).__init__(address)
+        super(TryExceptEntryBase, self).__init__(address)
 
     def apply_to_database(self, target, handler=None):
-        super(TryExceptTableBase, self).apply_to_database()
+        super(TryExceptEntryBase, self).apply_to_database()
         if handler:
             except_str = '{:08X}'.format(handler & ~1)
         else:
@@ -176,15 +176,15 @@ class TryExceptTableBase(SEHTable):
                 except_str))
 
 
-class TryExceptTable(TryExceptTableBase):
+class TryExceptEntry(TryExceptEntryBase):
     '''Represents a __try/__except style SCOPE_TABLE'''
     def __init__(self, address):
-        super(TryExceptTable, self).__init__(address)
+        super(TryExceptEntry, self).__init__(address)
         self.handler = Dword(address + 8) + idaapi.get_imagebase()
         self.target = Dword(address + 12) + idaapi.get_imagebase()
 
     def apply_to_database(self):
-        super(TryExceptTable, self).apply_to_database(
+        super(TryExceptEntry, self).apply_to_database(
             self.target, self.handler)
         _make_references(self.address + 8, self.handler, 'Filter  ')
         _make_references(self.address + 12, self.target, 'ExpBody ')
@@ -195,26 +195,26 @@ class TryExceptTable(TryExceptTableBase):
                 self.target & ~1))
 
 
-class TryInvalidExceptTable(TryExceptTableBase):
+class TryInvalidExceptEntry(TryExceptEntryBase):
     '''Represents a __try/__except style SCOPE_TABLE w/ invalid filter'''
     def __init__(self, address):
-        super(TryInvalidExceptTable, self).__init__(address)
+        super(TryInvalidExceptEntry, self).__init__(address)
         self.target = Dword(address + 12) + idaapi.get_imagebase()
 
     def apply_to_database(self):
-        super(TryInvalidExceptTable, self).apply_to_database(self.target)
+        super(TryInvalidExceptEntry, self).apply_to_database(self.target)
         MakeDword(self.address + 8)
         _make_references(self.address + 12, self.target, 'ExpBody ')
 
 
-class TryFinallyTable(SEHTable):
+class TryFinallyEntry(SEHEntry):
     '''Represents a __try/__finally style SCOPE_TABLE'''
     def __init__(self, address):
-        super(TryFinallyTable, self).__init__(address)
+        super(TryFinallyEntry, self).__init__(address)
         self.handler = Dword(address + 8) + idaapi.get_imagebase()
 
     def apply_to_database(self):
-        super(TryFinallyTable, self).apply_to_database()
+        super(TryFinallyEntry, self).apply_to_database()
         _make_references(self.address + 8, self.handler, 'Finally ')
         MakeDword(self.address + 12)
         _append_comment(
